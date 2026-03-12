7d8062605d1b45f3ddf04f39c397417bb50b06e9
Oseimuohan Itua <106272505+OseimuohanI@users.noreply.github.com>
2026-03-12 22:27:09 +0100

    Stateless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
 
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->validateCsrfTokens(except: ['api/*']);
 
         $middleware->web(append: [
             HandleAppearance::class,
